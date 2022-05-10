c95845c5f5839dcb5f4f87fd6ac1995745578234
Ademario Junior <ademariofernandesramos@gmail.com>
2022-05-10 13:24:45 -0300

    Create the base of the project

---
+++ demobasic/src/test/java/com/springtutor/demobasic/DemobasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtutor.demobasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
